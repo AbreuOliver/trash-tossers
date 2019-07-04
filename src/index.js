90b5d7c41ed41e8b3f0491d695996d936b61faa2
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2019-07-04 15:29:29 -0400

    This commit should save header styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/* --- REACT --- */
+import React from "react";
+/* --- REACT DOM --- */
+import ReactDOM from "react-dom";
+/* --- BROWSER ROUTER --- */
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* --- STYLES --- */
+import "../src/Styles/index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from "./Components/App";
+
+/* --- SERVICE WORKER --- */
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById("root")
+);
+
+serviceWorker.register();
