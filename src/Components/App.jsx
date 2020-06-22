bba09473a186f2540bd65b028fc2bf7e51451b4e
AbreuOliver <oliverabreu@icloud.com>
2020-06-21 23:20:26 -0400

    Updated tagline

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -9,7 +9,7 @@ function App() {
 			<div className="header-area">
 				<h1 id="main-heading">Trash Tossers</h1>
 			</div>
-			<h3>Personal Trash Disposal</h3>
+			<h3>Doorstep Trash Removal</h3>
 			<div className="button-container">
 				<button>Learn More</button>
 				<button className="payment">Request Pickup</button>
