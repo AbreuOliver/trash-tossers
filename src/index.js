d7126b5ad3e89a88ada573c19d15811ceca98367
AbreuOliver <oliverabreu@icloud.com>
2020-06-21 23:26:04 -0400

    This commit should remove declarations of unused imports

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-/* --- REACT --- */
 import React from "react";
-/* --- REACT DOM --- */
 import ReactDOM from "react-dom";
-/* --- BROWSER ROUTER --- */
 import { BrowserRouter as Router } from "react-router-dom";
 
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
-
-import CheckoutForm from '../src/Components/CheckoutForm';
-
-/* --- STYLES --- */
-import "../src/Styles/index.css";
+// import {Elements} from '@stripe/react-stripe-js';
+// import {loadStripe} from '@stripe/stripe-js';
+// import CheckoutForm from '../src/Components/CheckoutForm';
 
 import App from "./Components/App";
-
-/* --- SERVICE WORKER --- */
 import * as serviceWorker from "./serviceWorker";
-
+import "../src/Styles/index.css";
 
 
 ReactDOM.render(
